28121275fe88485d86e5aee05a2d753b2ed5a903
Ethan Husband <ethan.husband@evup.com.au>
2022-12-31 16:22:52 +1100

    add vercel analytics to code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import SitePage from '../components/SitePage'
-
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SitePage>
       <Component {...pageProps} />
+      <Analytics />
     </SitePage>
   )
 }
