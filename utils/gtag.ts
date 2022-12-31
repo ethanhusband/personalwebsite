6f677eb3b6952ec1820292d3ffea8606c71ef5aa
Ethan Husband <ethan.husband@evup.com.au>
2022-12-31 16:12:05 +1100

    fix ga pageview names

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -2,8 +2,9 @@ export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
+  console.log("Pageview for", `${url}`, typeof url);
   (window as any).gtag("config", GA_TRACKING_ID, {
-    page_path: url
+    page_path: `${url}`
   });
 };
 
