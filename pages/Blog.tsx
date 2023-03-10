86599674ea22c96d76c79c47c38775671bc480d0
Ethan Husband <ethan.husband@evup.com.au>
2023-03-10 21:31:34 +1100

    Add Resume

--- pages/Blog.tsx
+++ pages/Blog.tsx
@@ -6,17 +6,16 @@ export const BlogMenu: NextPage = () => {
     <div className="gap-y-6 flex flex-col">
       <EVChargerDLBSummary />
       {/** 
-              <li>How Quantum Computers Mark Solutions</li>
-              <li>Classing Holomorphic Complex Functions</li>
-              <li>How Is e Irrational?</li>
-              <li>The Fairness of Allocating To a Prime Number of People</li>
+       * Fair DLB     
+       * Automated Stripe Disbursements
+       * Classing Holomorphic Complex Functions
               
                * How To Setup Prisma and PlanetScale SQL databases
                * How To Setup an independent GraphQL server
                * Understanding the Riemann Zeta conjecture (prime number proof, all zeros in [0,1] proof)
                * The Beauty of Complex Numbers
-               * Optical Transistors
                * Application Course (gql, react, terraform)
+               * Handshake design pattern
                */}
     </div>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import SitePage from '../components/SitePage'
 import { Analytics } from '@vercel/analytics/react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  if (router.asPath === '/resume') {
+    return (
+      <iframe
+        className="w-full h-screen "
+        src="/assets/pdfs/Ethan_Husband_Resume.pdf"
+      />
+    )
+  }
+
   return (
     <SitePage>
       <Component {...pageProps} />
