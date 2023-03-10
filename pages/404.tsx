f565ba16b3772b3916b7b52f71eac259ec5645ca
Ethan Husband <ethan.husband@evup.com.au>
2023-03-10 22:50:40 +1100

    Resume redirect

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function FourZeroFour() {
+  const router = useRouter()
+
+  if (router.asPath === '/resume') {
+    router.push('https://resume.io/r/ISDvy31OM')
+  }
+
   return (
     <div className="bg-zinc-200 p-4 mt-16 rounded-lg  flex flex-col gap-y-2 mx-auto w-64 border-2 border-black ">
       <h1 className="text-center text-black">Error 404 - Page Not Found</h1>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,8 @@ import type { AppProps } from 'next/app'
 import SitePage from '../components/SitePage'
 import { Analytics } from '@vercel/analytics/react'
 import { useRouter } from 'next/router'
-import { redirect } from 'next/navigation'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  if (router.asPath === '/resume') {
-    redirect('https://resume.io/r/ISDvy31OM')
-    /*return (
-      <iframe
-        className="w-full h-screen"
-        allow="true"
-        src="/assets/pdfs/Ethan_Husband_Resume.pdf"
-      />
-    )*/
-  }
-
   return (
     <SitePage>
       <Component {...pageProps} />
